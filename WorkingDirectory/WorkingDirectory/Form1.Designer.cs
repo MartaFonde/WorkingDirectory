93f5bca714105f35aa750d5e1d96aeea86f72174
martafonde <marta4510@hotmail.com>
2020-11-08 19:09:21 +0100

    v1

---
+++ WorkingDirectory/WorkingDirectory/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace Ejer4Tema3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(40, 47);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(319, 20);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button1.Location = new System.Drawing.Point(393, 44);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Info";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(40, 92);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox2.Size = new System.Drawing.Size(607, 270);
+            this.textBox2.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(40, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(355, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Introduce ruta del directorio de trabajo. Puede ser una variable de entorno";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.Color.Red;
+            this.label2.Location = new System.Drawing.Point(508, 50);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 13);
+            this.label2.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(745, 429);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Set Current Directory";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ WorkingDirectory/WorkingDirectory/Form1.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ejer4Tema3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string chDir="";
+            if(textBox1.Text.StartsWith("%") && textBox1.Text.EndsWith("%"))
+            {
+                string enVar = textBox1.Text.Substring(1, textBox1.Text.Length - 2);
+                try
+                {
+                    chDir = Environment.GetEnvironmentVariable(enVar);
+                }
+                catch (ArgumentNullException)
+                {
+                    label2.Text = "Variable de entorno no encontrada";
+                }
+                catch (SecurityException)
+                {
+                    label2.Text = "Acceso no autorizado";
+                }
+            }
+            else
+            {
+                chDir = textBox1.Text;
+            }
+            if (ComprobarDirectorio(chDir))
+            {
+                bool valido = false;
+                DirectoryInfo d = null;
+                label2.Text = "";
+                try
+                {
+                    valido = true;
+                    d = new DirectoryInfo(Directory.GetCurrentDirectory());
+                }
+                catch (UnauthorizedAccessException)
+                {
+                    label2.Text = "Acceso no autorizado";
+                    valido = false;
+                }
+                if (valido)
+                {
+                    textBox2.Text = "SUBDIRECTORIOS\r\n";
+                    if (d.GetDirectories().Length > 0)
+                    {
+                        foreach (DirectoryInfo dir in d.GetDirectories())
+                        {
+                            textBox2.Text += dir.Name + "\r\n";
+                        }
+                    }
+                    else
+                    {
+                        textBox2.Text += "No contiene subdirectorios";
+                    }
+
+
+                    textBox2.Text += "\r\n \r\nARCHIVOS\r\n";
+                    if (d.GetFiles().Length > 0)
+                    {
+                        foreach (FileInfo arch in d.GetFiles())
+                        {
+                            textBox2.Text += arch.Name + "\r\n";
+                        }
+                    }
+                    else
+                    {
+                        textBox2.Text += "No contiene archivos";
+                    }
+                }                                
+            }            
+        }
+
+        private bool ComprobarDirectorio(string chDir)
+        {
+            try
+            {
+                Directory.SetCurrentDirectory(chDir);
+
+            }
+            catch (DirectoryNotFoundException)
+            {
+                label2.Text = "Directorio no encontrado";
+                return false;
+            }
+            catch (SecurityException)
+            {
+                label2.Text = "No tienes el permiso requerido";
+                return false;
+            }
+            catch (ArgumentException)
+            {
+                label2.Text = "Ruta no válida";
+                return false;
+            }
+            catch (IOException ex)
+            {
+                label2.Text = "Error: " + ex.Message;
+                return false;
+            }
+            return true;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {            
+            if(textBox1.TextLength == 0)
+            {
+                label2.Text = "";
+            }
+        }
+    }
+}
